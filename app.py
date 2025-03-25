2234078ee6a0b627c58d1bbb16588ed528932f48
ScottK156 <scottk8@nycstudents.net>
2025-03-25 09:24:02 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,17 @@
 mode = input("What mode would you like to use?")
 
+class flash_cards:
+    def __init__(self,key,value):
+        self.key = key
+        self.value = value
+    def harold(self):
+        return f"{self.key}"
+my_card = flash_cards["science":"hello"]
+print(my_card.harold)
 
+    
+        
 #Student Mode
-class Student:
-    def
- 
+
+
 #Teacher Mode
